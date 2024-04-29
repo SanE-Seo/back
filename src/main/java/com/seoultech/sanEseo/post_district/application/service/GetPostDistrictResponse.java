c12417d08dbb60ba7c38f089aba6b09cadfba148
changha <mentenseoul@gmail.com>
2024-04-29 16:21:27 +0900

    refactor: 카드 리스트 id추가

--- src/main/java/com/seoultech/sanEseo/post_district/application/service/GetPostDistrictResponse.java
+++ src/main/java/com/seoultech/sanEseo/post_district/application/service/GetPostDistrictResponse.java
@@ -6,9 +6,10 @@ import org.springframework.util.Assert;
 
 import java.util.List;
 
-public record GetPostDistrictResponse(List<GetImageResponse> postImages, String title, String subTitle, String time, int likes, String distance, String level, String districts) {
+public record GetPostDistrictResponse(Long postId, List<GetImageResponse> postImages, String title, String subTitle, String time, int likes, String distance, String level, String districts) {
 
     public GetPostDistrictResponse {
+        Assert.notNull(postId, "게시글 ID는 필수입니다.");
         Assert.hasText(title, "제목은 필수입니다.");
         Assert.hasText(subTitle, "부제목은 필수입니다.");
         Assert.hasText(time, "소요시간은 필수입니다.");
