8fd51fda67a34bd63a7750a613efbd35a29943c6
Hong-Mu <hongmuchae@gmail.com>
2024-05-01 04:12:44 +0900

    chore: 중복 제거

--- src/main/java/com/seoultech/sanEseo/global/config/DataSeoulAPI.java
+++ src/main/java/com/seoultech/sanEseo/global/config/DataSeoulAPI.java
@@ -7,15 +7,6 @@ import org.springframework.web.service.annotation.GetExchange;
 
 public interface DataSeoulAPI {
 
-    @GetExchange("/1360000/VilageFcstInfoService_2.0/getVilageFcst?serviceKey={serviceKey}&pageNo=1&numOfRows=1000&dataType=json&base_date={base_date}&base_time={base_time}&nx={nx}&ny={ny}")
-    WeatherAPIResponse getWeather(
-            @PathVariable("serviceKey") String serviceKey,
-            @PathVariable("base_date") String baseDate,
-            @PathVariable("base_time") String baseTime,
-            @PathVariable("nx") int nx,
-            @PathVariable("ny") int ny
-    );
-
     @GetExchange("/{serviceKey}/json/ListAirQualityByDistrictService/1/1/{code}/")
     PollutionAPIResponse getPollution(
             @PathVariable("serviceKey") String serviceKey,
